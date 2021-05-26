15099e340771c4bd1cbd838ad5e2bb1af9ee342f
Ali <aliazawi@hotmail.com>
2021-05-26 21:20:46 +0200

    2605-21:20

--- My_Django/first_project/first_app/views.py
+++ My_Django/first_project/first_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Webpage,AccessRecord
 # Create your views here.
 
 def index(request):
     #Comment here
-    #return HttpResponse("Ali")
-    my_dict = {"insert_me":"Hello I am from views.py !"}
-    return render(request,'first_app/index.html',context=my_dict)
+   # return HttpResponse("Ali")
+
+    #my_dict = {"insert_me":"Hello I am from views.py !"}
+    #return render(request,'first_app/index.html',context=my_dict)
+    Webpages= Webpage.objects.all()
+    AccessRecords=AccessRecord.objects.all()
+    my_dict = {'Webpages':Webpages,'AccessRecords':AccessRecords}
+    return render(request,'first_app/index.html',my_dict)
